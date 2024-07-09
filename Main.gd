ab3eceb34177b2c0506f52c70b03918f2b04822b
SeppNel <35899928+SeppNel@users.noreply.github.com>
2024-07-09 10:18:22 +0200

    Initial Implementation of High Score

--- Main.gd
+++ Main.gd
@@ -2,12 +2,14 @@ extends Node
 
 @export var mob_scene: PackedScene
 var score
+var highScore = 0
 var bigPenguinChance = 10
 var difficultyFactor = 0
+var highScoreUpdated : Bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	load()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -22,8 +24,12 @@ func game_over():
 	$Music.stop()
 	$DeathSound.play()
 
+	if highScoreUpdated:
+		save()
+
 func new_game():
 	score = 0
+	highScoreUpdated = false
 	difficultyFactor = 0
 	$Player.start($StartPosition.position)
 	
@@ -56,9 +62,15 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+
 	if score % 50 == 0:
 		difficultyFactor += 1
 		$MobTimer.wait_time -= 0.01
+	
+	if score > highScore:
+		highScore = score
+		highScoreUpdated = true
+
 	$HUD.update_score(score)
 
 
@@ -72,3 +84,31 @@ func _on_player_touch(pos):
 		$HUD.show_origin_marker(pos)
 	else:
 		$HUD.hide_origin_marker()
+
+func save():
+	var save_dict = {
+		"high_score" : highScore,
+	}
+
+	var save_file = FileAccess.open("user://savegame.save", FileAccess.WRITE)
+	var json_string = JSON.stringify(save_dict)
+	save_file.store_line(json_string)
+
+
+func load():
+	if not FileAccess.file_exists("user://savegame.save"):
+		return # Error! We don't have a save to load.
+
+	var save_file = FileAccess.open("user://savegame.save", FileAccess.READ)
+	var json_string = save_file.get_line()
+
+	# Creates the helper class to interact with JSON
+	var json = JSON.new()
+
+	# Check if there is any error while parsing the JSON string
+	if not json.parse(json_string) == OK:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+
+	# Get the data from the JSON object
+	var data = json.get_data()
+	highScore = data["high_score"]
